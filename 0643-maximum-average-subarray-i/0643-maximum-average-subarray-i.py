d358bfe97446108699805714eaa6166db4249647
Spandana Bn <spandanabn1620@gmail.com>
2026-09-03 20:45:50 +0530

    Time: 128 ms (17.56%), Space: 19.1 MB (18.68%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -1,12 +1,14 @@
-class Solution:
-    def findMaxAverage(self, nums: List[int], k: int) -> float:
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        
         win_sum=sum(nums[:k])
-        win_avg=win_sum/k
+        win_avg=win_sum/float(k)
         max_avg=win_avg
         for i in range(len(nums)-k):
             win_sum=(win_sum-nums[i]+nums[i+k])
-            win_avg=win_sum/k
+            win_avg=win_sum/float(k)
             max_avg=max(max_avg,win_avg)
         return max_avg
+
 
         
